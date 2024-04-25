08ea192c6b4db390ff7aa207b70b0a4966386604
joel <joelnunes974@gmail.com>
2024-04-25 11:07:17 -0300

    Exception Handling

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,9 +1,10 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { UpdateMovieDto } from './dto/update-movie.dto';
 import { Repository } from 'typeorm';
 import { Movie } from './entities/movie.entity';
 import { InjectRepository } from '@nestjs/typeorm';
+import { HttpException } from '@nestjs/common';
 
 @Injectable()
 export class MoviesService {
@@ -13,33 +14,59 @@ export class MoviesService {
 
   }
   async create(createMovieDto: CreateMovieDto) {
-    const movie = this.moviesRepository.create
-    (createMovieDto)
-    return await this.moviesRepository.save(movie); 
+    try {
+      const movie = this.moviesRepository.create
+      (createMovieDto)
+      return await this.moviesRepository.save(movie);
+    } catch (error) {
+      console.log(error)
+      throw new HttpException('BadRequest', HttpStatus.BAD_REQUEST);
+    }
+     
   }
 
   async findAll() {
-    return await this.moviesRepository.find();
+    try {
+        return await this.moviesRepository.find();
+    } catch (error) {
+  console.log(error)
+  throw new HttpException('BadRequest', HttpStatus.BAD_REQUEST);
+    }
   }
 
   async findOne(id: number) {
+    try{ 
     return await this.moviesRepository.findOne({where: {id}})
+    }catch (error) {
+      console.log(error)
+      throw new HttpException('BadRequest', HttpStatus.BAD_REQUEST);
+        }
   }
 
   async update(id: number, updateMovieDto: UpdateMovieDto) {
+    try{
     const movie = await this.findOne(id)
     if(!movie) {
       throw new NotFoundException();
     }
     Object.assign(movie,updateMovieDto)
-    return await this.moviesRepository.save(movie)
+    return await this.moviesRepository.save(movie)}
+    catch (error) {
+      console.log(error)
+      throw new HttpException('BadRequest', HttpStatus.BAD_REQUEST);
+        }
   }
 
   async remove(id: number) {
+    try{
     const movie = await this.findOne(id)
       if(!movie) {
       throw new NotFoundException();
     }
     return await this.moviesRepository.remove(movie);
+  }catch (error) {
+    console.log(error)
+    throw new HttpException('BadRequest', HttpStatus.BAD_REQUEST);
+      }
   }
 }
